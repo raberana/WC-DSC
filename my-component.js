643e8517b0f4dd6d23c55a047f085e40e675c308
Roj Beraña <raberana@gmail.com>
2019-01-17 17:47:39 +0800

    Create my-component.js

---
+++ my-component.js
@@ -0,0 +1,16 @@
+class MyComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <style>
+          h3 {
+            font-style: italic;
+          }
+        </style>
+
+        <h1>Hello DSC!</h1>
+        <h3>Welcome to Web Components (from custom component)</h3>
+      `;
+  }
+}
+
+window.customElements.define('my-component', MyComponent);
